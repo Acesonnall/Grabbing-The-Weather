546623f39073c949afa044c7a26c1ad34620692d
Omar Taylor <omtaylor@iastate.edu>
2017-12-02 17:53:07 -0600

    Just learned how async-await works for ES7.

--- test/tests.js
+++ test/tests.js
@@ -20,37 +20,31 @@ describe('User', () => {
     });
 
     describe('GET /weather', () => {
-        it('should ask for city and country if both fields are empty', done => {
-            api.get('/weather?city=&country=')
+        it('should ask for city and country if both fields are empty', async () => {
+            const res = await api.get('/weather?city=&country=')
                 .set('Accept', 'application/json')
-                .expect(200)
-                .end(function (err, res) {
-                    expect(res.body.err).to.be.a('string');
-                    expect(res.body.err).to.equal('Please enter a city and country name.');
-                    done();
-                });
+                .expect(200);
+
+            expect(res.body.err).to.be.a('string');
+            expect(res.body.err).to.equal('Please enter a city and country name.');
         });
 
-        it('should ask for city if city field is empty', done => {
-            api.get('/weather?city=&country=China')
+        it('should ask for city if city field is empty', async () => {
+            const res = await api.get('/weather?city=&country=China')
                 .set('Accept', 'application/json')
-                .expect(200)
-                .end(function (err, res) {
-                    expect(res.body.err).to.be.a('string');
-                    expect(res.body.err).to.equal('Please enter a city name.');
-                    done();
-                });
+                .expect(200);
+
+            expect(res.body.err).to.be.a('string');
+            expect(res.body.err).to.equal('Please enter a city name.');
         });
 
-        it('should ask for country if country field is empty', done => {
-            api.get('/weather?city=Zhengzhou&country=')
+        it('should ask for country if country field is empty', async () => {
+            const res = await api.get('/weather?city=Zhengzhou&country=')
                 .set('Accept', 'application/json')
-                .expect(200)
-                .end(function (err, res) {
-                    expect(res.body.err).to.be.a('string');
-                    expect(res.body.err).to.equal('Please enter a country name.');
-                    done();
-                });
+                .expect(200);
+
+            expect(res.body.err).to.be.a('string');
+            expect(res.body.err).to.equal('Please enter a country name.');
         });
 
         it('should report location not found if input doesn\'t match database values', async () => {
@@ -62,19 +56,17 @@ describe('User', () => {
             expect(res.body.err).to.equal('Location not supported.');
         });
 
-        it('should report weather information if no prior errors', done => {
-            api.get('/weather?city=Zhengzhou&country=China')
+        it('should report weather information if no prior errors', async () => {
+           const res = await api.get('/weather?city=Zhengzhou&country=China')
                 .set('Accept', 'application/json')
-                .expect(200)
-                .end(function (err, res) {
-                    expect(res.body.info).to.have.property('city');
-                    expect(res.body.info.city).to.be.a('string');
-                    expect(res.body.info).to.have.property('fahrenheit');
-                    expect(res.body.info.fahrenheit).to.be.a('number');
-                    expect(res.body.info).to.have.property('celsius');
-                    expect(res.body.info.celsius).to.be.a('number');
-                    done();
-                });
+                .expect(200);
+
+            expect(res.body.info).to.have.property('city');
+            expect(res.body.info.city).to.be.a('string');
+            expect(res.body.info).to.have.property('fahrenheit');
+            expect(res.body.info.fahrenheit).to.be.a('number');
+            expect(res.body.info).to.have.property('celsius');
+            expect(res.body.info.celsius).to.be.a('number');
         });
     });
 });
